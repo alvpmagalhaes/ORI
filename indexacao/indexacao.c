8df2cee1911dcac6235018f487dc3d0650a665a0
Alvaro Magalhães <alvpmagalhaes@gmail.com>
2018-09-04 13:34:09 -0300

    Update indexacao.c

--- indexacao/indexacao.c
+++ indexacao/indexacao.c
@@ -6,6 +6,7 @@ As informações devem ser obtidas da entrada padrão
 */
 
 #include <stdio.h>
+#include <string.h>
 
 struct reg {
     int chave;
@@ -21,79 +22,102 @@ struct reg {
 struct cur {
     int atual;
     int proximo;
-}
+};
 
 struct ind {
     int chave;
     int posicao;
-}
+    char cidade[40];
+};
 
 int main () {
-    FILE *arquivo, *indices;
+    FILE *arquivo, *indices, *registros;
     struct reg registro;
     struct cur cursor;
     struct ind indice[20];
-    int i,max;
+    int i=0,max;
+
+    //Funcao de verificacao de existencia e quantidade de registros
+
+    indices = fopen("indices.bin", "wb");
+    if(indices == NULL)
+        perror("Erro ao abrir indices.bin");
+    else {
+        printf("entrooou\n");
+        /*while (fread(&indice[i].chave, sizeof indice[i].chave, 1, indices) > 0 &&
+        fread(&indice[i].posicao, sizeof indice[i].posicao, 1, indices) > 0 &&
+        fread(indice[i].cidade, sizeof indice[i].cidade, 1, indices) > 0){
+            printf("chave: %d\ncidade: %s\nposicao: %d\n\n", indice[i].chave, indice[i].cidade, indice[i].posicao);
+            i++;
+        }*/
+        
+    }
+
+    fclose(indices);
     
 
-    printf("1) Escrever\n2) Ler todos\nSelecione uma opcao: ");
+    printf("1) Escrever\n2) Ler todos\n3)Ler um registro\nSelecione uma opcao: ");
     scanf("%d", &i);
     if (i == 1) {
         arquivo = fopen("registros.bin", "wb");
         if(arquivo == NULL)
             perror("Erro ao abrir registros.bin");
         else {
-                printf("\nInsira o numero de registro que deseja escrever: ");
-                scanf("%d", &max);
+            printf("\nInsira o numero de registro que deseja escrever: ");
+            scanf("%d", &max);
+            getchar();
+            for (i=0;i<max;i++) {
+                // Coleta valores dos registros
+                printf("Ultimo nome: ");
+                scanf("%[^\n]s", registro.ultimonome);
                 getchar();
-                for (i=0;i<max;i++) {
-                    // Coleta valores dos registros
-                    printf("Ultimo nome: ");
-                    scanf("%[^\n]s", registro.ultimonome);
-                    getchar();
-                    printf("Primeiro nome: ");
-                    scanf("%[^\n]s", registro.primeironome);
-                    getchar();
-                    printf("Endereco: ");
-                    scanf("%[^\n]s", registro.endereco);
-                    getchar();
-                    printf("Cidade: ");
-                    scanf("%[^\n]s", registro.cidade);
-                    getchar();
-                    printf("Estado: ");
-                    scanf("%[^\n]s", registro.estado);
-                    getchar();
-                    printf("CEP: ");
-                    scanf("%[^\n]s", registro.cep);
-                    getchar();
-                    printf("Telefone: ");
-                    scanf("%[^\n]s", registro.telefone);
-                    getchar();
+                printf("Primeiro nome: ");
+                scanf("%[^\n]s", registro.primeironome);
+                getchar();
+                printf("Endereco: ");
+                scanf("%[^\n]s", registro.endereco);
+                getchar();
+                printf("Cidade: ");
+                scanf("%[^\n]s", registro.cidade);
+                getchar();
+                printf("Estado: ");
+                scanf("%[^\n]s", registro.estado);
+                getchar();
+                printf("CEP: ");
+                scanf("%[^\n]s", registro.cep);
+                getchar();
+                printf("Telefone: ");
+                scanf("%[^\n]s", registro.telefone);
+                getchar();
 
+                // Escreve registro no arquivo
+                printf("\n-->Numero de itens escritos: %ld\n", 
+                fwrite(&registro, sizeof registro, 1, arquivo));
+                
+                // Salva indices dos registros
+                indice[i].chave = i;
+                indice[i].posicao = ftell(arquivo);
+                strcpy (indice[i].cidade, registro.cidade);
+            }
+            printf("\n\nRegistros salvos com sucesso!\n");
+
+            fclose(arquivo);
+            
+            indices = fopen("indices.bin", "wb");
+            if(indices == NULL)
+                perror("Erro ao abrir indices.bin");
+            else {
+                for (i=0;i<max;i++) {
                     // Escreve registro no arquivo
                     printf("\n-->Numero de itens escritos: %ld\n", 
-                    fwrite(&registro, sizeof registro, 1, arquivo));
+                    fwrite(&indice[i], sizeof indice, 1, indices));
                     
-                    // Salva indices dos registros
-                    indice[i].chave = i;
-                    indice[i].cursor = ftell(arquivo);
                 }
-                printf("\n\nRegistros salvos com sucesso!\n");
-
-                indices = fopen("indices.bin", "wb");
-                if(indices == NULL)
-                    perror("Erro ao abrir indices.bin");
-                else {
-                    for (i=0;i<max;i++) {
-                        // Escreve registro no arquivo
-                        printf("\n-->Numero de itens escritos: %ld\n", 
-                        fwrite(&indice[i], sizeof indice, 1, indices));
-                    }
-                    printf("\n\nIndices salvos com sucesso!\n");
-                }
-
-                fclose(indices);
+                printf("\n\nIndices salvos com sucesso!\n");
             }
+
+            fclose(indices);
+            
         }
     } else {
         arquivo = fopen("registros.bin", "rb"); // acesso de leitura
@@ -104,9 +128,10 @@ int main () {
             while(fread(&registro, sizeof registro, 1, arquivo) > 0)
                 printf("%s %s\n", registro.primeironome, registro.ultimonome);
         }
+        fclose(registros);
     }
-
-	fclose(arquivo);
+	
+	
 	
     return 0;
 }
